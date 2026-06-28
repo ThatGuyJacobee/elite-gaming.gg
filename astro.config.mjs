04b288e9be90bab36d7967edb55ad45924859d01
ThatGuyJacobee <42719082+ThatGuyJacobee@users.noreply.github.com>
2026-06-28 15:29:34 +0100

    style: cleanup astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://elite-gaming.gg",
-
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react(), icon()]
+    site: "https://elite-gaming.gg",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [react(), icon()],
 });
